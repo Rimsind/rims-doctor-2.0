c7fdedcda5d5e26aa880c868a9fb56cc54e128e7
Rabindranath Mandal <rimstech21@gmail.com>
2022-02-11 11:08:20 +0530

    Login Part Update

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,20 @@
 const Login = () => {
-  return <></>;
+  return (
+    <>
+      <div className="login-form-main">
+        <div className="container">
+          <div className="row">
+            <div className="col-4">
+              <p>Hello World</p>
+            </div>
+            <div className="col-8">
+              <p>Hello World</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
