827c1e74f2a62ba8ccf72a0504ca3b6df6616fd3
Rims <83367739+Rimsind@users.noreply.github.com>
2022-03-01 20:06:24 -0800

    8787

--- pages/report/MedicalHistory.js
+++ pages/report/MedicalHistory.js
@@ -208,7 +208,7 @@ const MedicalHistory = () => {
                                 </p>
                               </div>
                               <div className="col-md-4 inner_column">
-                                <p className="fs-6">Master's Degree</p>
+                                <p className="fs-6">Master&apos;s Degree</p>
                               </div>
                             </div>
                           </div>
